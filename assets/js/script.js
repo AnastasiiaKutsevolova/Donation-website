b26a3df4b07bd91de6b70a7536abd8364c72bc05
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-05-03 21:34:10 -0600

    set active link on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const volunteerLoginBtn = document.getElementById("volunteer-login-btn");
 
   const getUser = JSON.parse(localStorage.getItem("user")) || {};
+
+  const sections = document.querySelectorAll("section");
+  const navLi = document.querySelectorAll("nav ul li");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (scrollY >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLi.forEach((li) => {
+      li.classList.remove("active");
+
+      if (li.classList.contains(current)) {
+        li.classList.add("active");
+      }
+    });
+  });
+
   //   Render Volunteers
 
   const getVolunteers = async () => {
